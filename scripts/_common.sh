6b7f3764fcabc0feefefb7a5db6606f192603cc5
psychozoic <psychozoic@gmail.com>
2026-04-18 18:54:02 +0200

    feat: initial implementation of OpenVPN server YunoHost package with LDAP authentication and configurable network settings

---
+++ scripts/_common.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+# OpenVPN server settings
+app=$YNH_APP_ID
+ovpn_dir="/etc/openvpn/server/$app"
+easy_rsa_dir="/etc/openvpn/easy-rsa/$app"
